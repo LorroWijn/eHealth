811b09f7799737cd032d701ac5943f0a11bcd3f2
DZoetmulder <36473983+DZoetmulder@users.noreply.github.com>
2020-10-09 14:52:41 +0200

    Added bootstrap and picture

--- eHealth/settings.py
+++ eHealth/settings.py
@@ -57,6 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'static'),
             ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+] 
